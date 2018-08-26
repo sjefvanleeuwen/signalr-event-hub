2fc437dd956b66b8aedf7b690c6b54401c7c56fe
Sjef van LEeuwen <sjef.van.leeuwen@live.nl>
2018-08-26 02:32:27 +0200

    initial commit

---
+++ src/signalr-event-hub/Hubs/EventHub.cs
@@ -0,0 +1,36 @@
+using System;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Cors;
+using Microsoft.AspNetCore.SignalR;
+
+namespace signalr_event_hub.Hubs
+{
+     [EnableCors("MyPolicy")]
+    public class EventHub : Hub
+    {
+        public async Task Subscribe(string topic)
+        {
+            await Groups.AddToGroupAsync(Context.ConnectionId, topic);
+            await Clients.Caller.SendAsync("subscribe",topic);
+            Console.WriteLine("subscribed..");
+        }
+        public void Unsubscribe(string topic)
+        {
+            Groups.RemoveFromGroupAsync(Context.ConnectionId, topic);
+            System.Diagnostics.Trace.WriteLine("unsubscribed..");
+        }
+
+        public void Echo(string echo)
+        {
+            Console.WriteLine($"echo: {echo}");
+            Clients.All.SendAsync("echo",echo);
+        }
+
+        public async Task PublishMessage(string topic, string message)
+        {
+            await Clients.Group(topic).SendAsync("publishmessage",topic,message);
+          //  await Clients.Caller.SendAsync("publishmessage",topic,message);
+            Console.WriteLine($"{topic}:{message}");
+        }
+    }
+}
---
+++ src/signalr-test-client/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.SignalR.Client;
+
+namespace signalr_test_client
+{
+    class Program
+    {
+        static HubConnection connection;
+
+        public static async Task<int> Main(string[] args)
+        {
+           await t();
+           return 0;
+        }
+
+        static async Task t(){
+            connection = new HubConnectionBuilder()
+                .WithUrl("http://localhost:5000/eventhub")
+                .Build();
+            connection.On<string, string>("publishmessage", (topic, message) =>
+            {
+                Console.WriteLine($"{topic}: {message}");
+            });
+            connection.On<string>("echo", (echo) =>
+            {
+                Console.WriteLine($"echo: {echo}");
+            });
+  
+            connection.On<string>("subscribe", (echo) =>
+            {
+                Console.WriteLine($"subscribed: {echo}");
+            });
+
+            await connection.StartAsync();
+
+            Console.WriteLine("started...");
+
+            var x = new Random().Next();
+            while (true)
+            {
+
+                await connection.InvokeAsync("Subscribe", "topic");
+                await connection.InvokeAsync("PublishMessage","topic", "Hello World! " + x);
+                await connection.InvokeAsync("Echo","Hello World! " + x);
+                Task.Delay(TimeSpan.FromSeconds(10)).GetAwaiter().GetResult();
+                x++;
+            }
+        }
+    }
+}
