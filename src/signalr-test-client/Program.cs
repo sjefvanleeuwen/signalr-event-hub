c487e9feb1ebddfde6b53b4f35fa6b71d6d38f63
Sjef van Leeuwen <sjef.van.leeuwen@live.nl>
2018-10-02 23:47:05 +0200

    reinstate task polling

--- src/signalr-test-client/Program.cs
+++ src/signalr-test-client/Program.cs
@@ -73,42 +73,42 @@ namespace signalr_test_client
             Console.ReadLine();
 
             // await connection.InvokeAsync("subscribe","topic2");
-            // var x = new Random().Next();
-            // var lastQuery = DateTime.MinValue;
-            // var total=0;
-            // DateTime lastQueryExecution = DateTime.MinValue;
-            // while (true)
-            // {
-            //     if (lastQuery != DateTime.MinValue)
-            //         lastQueryExecution = DateTime.Now;
-            //     var externalTaskQuery = new TaskQuery() { Active = true,  /* broken: CreatedAfter = DateTime.Now */};
-            //     // add some sorting
-            //     externalTaskQuery.Sort(TaskSorting.Created, SortOrder.Descending);
-            //     // request external tasks according to query
-            //     List<UserTaskInfo> tasks = await camunda.UserTasks.Query(externalTaskQuery).List();
-            //     var q = tasks.Where(p=>p.Created>=lastQuery);
-            //     total+=q.Count();
-            //     if (q.Count()>0){
-            //         var i=0;
-            //         Console.WriteLine($"publish message {q.Count()} new messages out of {total}.");
-            //         // only do sparse queries for now.
-            //         if (q.Count()<10){
-            //             foreach (var task in q) {
-            //                 i++;
-            //                 Console.WriteLine($"pushing task id ${task} process data {i} out of {q.Count()}.");
-            //                 connection.InvokeAsync("PublishMessage","human-task-data","info","",JsonConvert.SerializeObject(task));
-            //             }
-            //             Console.WriteLine("showing the first task for debug purposes");
-            //             Console.WriteLine("-----------------------------------------");
-            //             Console.WriteLine(JsonConvert.SerializeObject(q.First()));
-            //             Console.WriteLine("-----------------------------------------");
-            //         }
-            //         await connection.InvokeAsync("PublishMessage","dashboard-human-tasks",q.Count(),q.Count(),"");
-            //     }
-            //     lastQuery=DateTime.Now;
-            //     Task.Delay(TimeSpan.FromSeconds(20)).GetAwaiter().GetResult();
-            //     x++;
-            // }
+            var x = new Random().Next();
+            var lastQuery = DateTime.MinValue;
+            var total=0;
+            DateTime lastQueryExecution = DateTime.MinValue;
+            while (true)
+            {
+                if (lastQuery != DateTime.MinValue)
+                    lastQueryExecution = DateTime.Now;
+                var externalTaskQuery = new TaskQuery() { Active = true,  /* broken: CreatedAfter = DateTime.Now */};
+                // add some sorting
+                externalTaskQuery.Sort(TaskSorting.Created, SortOrder.Descending);
+                // request external tasks according to query
+                List<UserTaskInfo> tasks = await camunda.UserTasks.Query(externalTaskQuery).List();
+                var q = tasks.Where(p=>p.Created>=lastQuery);
+                total+=q.Count();
+                if (q.Count()>0){
+                    var i=0;
+                    Console.WriteLine($"publish message {q.Count()} new messages out of {total}.");
+                    // only do sparse queries for now.
+                    if (q.Count()<10){
+                        foreach (var task in q) {
+                            i++;
+                            Console.WriteLine($"pushing task id ${task} process data {i} out of {q.Count()}.");
+                            connection.InvokeAsync("PublishMessage","human-task-data","info","",JsonConvert.SerializeObject(task));
+                        }
+                        Console.WriteLine("showing the first task for debug purposes");
+                        Console.WriteLine("-----------------------------------------");
+                        Console.WriteLine(JsonConvert.SerializeObject(q.First()));
+                        Console.WriteLine("-----------------------------------------");
+                    }
+                    await connection.InvokeAsync("PublishMessage","dashboard-human-tasks",q.Count(),q.Count(),"");
+                }
+                lastQuery=DateTime.Now;
+                Task.Delay(TimeSpan.FromSeconds(20)).GetAwaiter().GetResult();
+                x++;
+            }
         }
     }
 }
